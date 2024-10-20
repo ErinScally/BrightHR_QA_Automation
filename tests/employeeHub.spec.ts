112b46418c8503e1ac62af6703617829a83b3716
ErinScally <erin.batman@yahoo.com>
2024-10-20 20:11:58 +0100

    Tidy up tests

--- tests/employeeHub.spec.ts
+++ tests/employeeHub.spec.ts
@@ -19,7 +19,7 @@ test.beforeAll(async ({browser}) =>{
 //     await page.close();
 // });
 
-test('Add employee with full details', async () => {
+test('Add employee with full details and verify fields', async () => {
     //employee creation
     await page.getByRole('button', { name: 'Add employee' }).click();
     await page.getByLabel('First name').click();
@@ -44,7 +44,8 @@ test('Add employee with full details', async () => {
     await expect(page.getByLabel('Job title(optional)')).not.toBeEmpty();
 });
 
-test('Add employee plus third from modal', async ({ page }) => {
+test('Add additional employee via modal', async ({ page }) => {
+    //creation initial employee
     await page.getByRole('button', { name: 'Add employee' }).click();
     await page.getByLabel('First name').click();
     await page.getByLabel('First name').fill('Another');
@@ -53,6 +54,8 @@ test('Add employee plus third from modal', async ({ page }) => {
     await page.getByLabel('Email address').click();
     await page.getByLabel('Email address').fill('employeetwo@test.com');
     await page.getByRole('button', { name: 'Save new employee' }).click();
+
+    //creation of additional employee via modal
     await page.getByRole('button', { name: 'Add another employee' }).click();
     await page.getByLabel('First name').click();
     await page.getByLabel('First name').fill('Third');
@@ -61,19 +64,22 @@ test('Add employee plus third from modal', async ({ page }) => {
     await page.getByLabel('Email address').click();
     await page.getByLabel('Email address').fill('employeethree@test.com');
     await page.getByRole('button', { name: 'Save new employee' }).click();
-    await page.getByLabel('Close modal').click();
+
+    //validate additional employee was created and all fields are set
+    await page.getByRole('link', { name: 'Go to profile' }).click();
+    await expect(page.getByLabel('First name')).not.toBeEmpty();
+    await expect(page.getByLabel('Last name')).not.toBeEmpty();
+    await expect(page.getByLabel('Email address')).not.toBeEmpty();
+    await expect(page.getByLabel('Phone number(optional)')).not.toBeEmpty();
+    await expect(page.getByLabel('Job title(optional)')).not.toBeEmpty();
 });
 
-test('Assert three employees present', async ({ page }) => {
-    // await page.goto('https://sandbox-app.brighthr.com/lite');
-    // await page.getByRole('link', { name: 'Log in' }).click();
-    // await page.getByLabel('Email address').click();
-    // await page.getByLabel('Email address').fill('erin.batman@yahoo.com');
-    // await page.getByLabel('Password').click();
-    // await page.getByLabel('Password').fill('Testpassword1');
-    // await page.getByRole('button', { name: 'Login' }).click();
+test('Validate all created employees are present', async ({ page }) => {
+    //navigate to employee tab
     await page.getByTestId('sideBar').getByRole('link', { name: 'Employees' }).click();
-    await expect(page.locator('text=ETTest Employee')).toBeTruthy;
+
+    //validate three profiles exist
+    await expect(page.locator('text=EOEmployee One')).toBeTruthy;
     await expect(page.locator('text=AEAnother Employee')).toBeTruthy;
     await expect(page.locator('text=TEThird Employee')).toBeTruthy;
 });
